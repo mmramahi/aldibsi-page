4b8232dcfcea460430adb7b7be0fb8e55c308949
mmramahi <mmramahi@yahoo.com>
2021-06-15 16:13:24 +0300

    form-modifications

--- src/components/FormContact.js
+++ src/components/FormContact.js
@@ -15,7 +15,12 @@ const FormConract = () => {
               We are here to help, send your project details and an expert will
               contact you
             </h4>
-            <Form name="contact" netlify>
+            <Form
+              name="contact"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Name</Form.Label>
                 <Form.Control name="name" type="text" placeholder="Your Name" />
